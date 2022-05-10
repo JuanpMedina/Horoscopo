28e7039919725558bd5b79ee1315b6e6af1526c7
Juan Pablo Medina <Juampamedinadiaz2002@gmail.com>
2022-05-10 09:33:32 -0500

    Inicializacion proyecto Horoscopo

---
+++ src/horoscopo/Horoscopo.java
@@ -0,0 +1,15 @@
+package horoscopo;
+/**
+ *
+ * @author jpmd2
+ */
+public class Horoscopo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
